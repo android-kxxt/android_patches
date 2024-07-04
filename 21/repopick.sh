e34755f59df85f093781a566193926e7ada616c2
kxxt <rsworktech@outlook.com>
2024-07-04 22:47:36 +0800

    Update picks for 21

--- 21/repopick.sh
+++ 21/repopick.sh
@@ -4,16 +4,8 @@ set -e
 source "build/envsetup.sh";
 source "vendor/lineage/build/envsetup.sh";
 
-# device/qcom/sepolicy_vndr/sm8450
-changes=(
-383884 # sepolicy_vndr: update sepolicy for health HAL service
-)
-repopick -P device/qcom/sepolicy_vndr/sm8450 ${changes[@]}&
-
 # hardware/xiaomi
 changes=(
-385354 # vintf: Add fingerprint extension to common fcm
-385355 # vintf: Add fpc and goodix 1.0 extensions to fcm
 352657 # Add dummy sensors sub HAL
 352658 # sensors: Make sensor batch function virtual
 352659 # sensors: Make sensor run function virtual
@@ -21,10 +13,23 @@ changes=(
 352661 # sensors: Make sensor set mode operation function virtual
 352662 # sensors: Move one shot sensor out of main class
 352663 # sensors: Fix locking around setOperationMode and activate
-352664 # sensors: Add udfps long press sensor
+352664 # sensors: Create sysfs polling one shot sensor
+392967 # sensors: Let the reading of poll fd be configurable
+392968 # sensors: Add SysfsPollingOneShotSensor constructor without enable path
+392969 # sensors: Add udfps long press sensor using SysfsPollingOneShotSensor
 352665 # sensors: Handle fod_pressed_state without coordinates
+363160 # hidl: biometrics: fingerprint: Add enroll methods to udfps handler
+392965 # vibrator: effect: Create double click effect from click if necessary
+392966 # vibrator: effect: Fallback to click if an effect is missing
 )
 repopick -P hardware/xiaomi ${changes[@]}&
+
+# frameworks/base
+changes=(
+386158 # Add 5G Ultra Wideband icon carrier config keys
+386159 # Fix default values for 5G Ultra Wideband icon carrier config keys
+)
+repopick -P frameworks/base ${changes[@]}&
 
 # vendor/lineage
 changes=(
@@ -32,10 +37,8 @@ changes=(
 )
 repopick -P vendor/lineage ${changes[@]}&
 
+
 repopick -P frameworks/base -f -t 21-snet
 
 wait
 
-cd hardware/qcom-caf/sm8450/display
-git pull https://github.com/LineageOS/android_hardware_qcom_display refs/changes/99/384299/1
-cd ../../../../
