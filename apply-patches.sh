22ffe6bafd4438126616fde1a3c7902b674526cc
kxxt <rsworktech@outlook.com>
2025-06-15 16:55:43 +0800

    Check if patch is already applied using change ids from last 50 commits
    Change-Id: I6f1be1bb1250482396857bcd23182a4a57084e40

--- apply-patches.sh
+++ apply-patches.sh
@@ -29,7 +29,7 @@ msg "Applying patches..."
 # If 3-way am succeeded, notify user by issuing a warning.
 function git_am() {
     if [[ $# -ne 2 ]]; then
-        error ""
+        error "usage: $FUNCNAME DIR PATCH"
     fi
     local workdir="$1"
     local patch="$2"
@@ -45,23 +45,41 @@ function git_am() {
     fi
 }
 
-find "$SCRIPT_DIR/$SUBDIR" -type f -name "*.patch" | while read patch; do
-    relative="$(realpath --relative-to="$SCRIPT_DIR/$SUBDIR" "$patch")"
-    dir="$(dirname "$relative")"
-    msg "Applying $relative to $dir"
-    output="$(patch -Np1 -d "$TOP/$dir" --dry-run < "$patch" || true)"
-    if ! grep -q "hunks\? FAILED" <<< "$output"; then
-        # no hunk FAILED
-        if grep -q "hunks\? ignored" <<< "$output"; then
-            # some or all hunks are ignored
-            warning "Some/All hunks are ignored when trying to applying $relative, skipping this patch"
-        else
-            git_am "$TOP/$dir" "$patch"
-        fi
-    else
-        # Some hunks failed
-        error "failed to apply $patch!"
-        echo "$output"
-        exit 3
+
+# Get the already picked change ids of a given repo
+function get_already_picked_change_ids() {
+    if [[ $# -ne 1 ]]; then
+        error "usage: $FUNCNAME DIR"
     fi
+    git -C "$1" rev-list --header --max-count 50 HEAD | grep -a Change-Id: | xargs
+}
+
+ find "$SCRIPT_DIR/$SUBDIR" -name '*.patch'  -printf '%h\n' | sort -u | while read patchdir; do
+    dir="$(realpath --relative-to="$SCRIPT_DIR/$SUBDIR" "$patchdir")"
+    applied_changes="$(get_already_picked_change_ids "$TOP/$dir")"
+    # msg2 "patchdir = $patchdir, dir = $dir"
+    find "$patchdir" -type f -name "*.patch" | while read patch; do
+        relative="$(realpath --relative-to="$SCRIPT_DIR/$SUBDIR" "$patch")"
+        change_id="$(grep Change-Id: "$patch" | xargs)"
+        if [[ $applied_changes == *$change_id* ]]; then
+            msg "Skipping already applied $relative"    
+            continue
+        fi
+        msg "Applying $relative to $dir"
+        output="$(patch -Np1 -d "$TOP/$dir" --dry-run < "$patch" || true)"
+        if ! grep -q "hunks\? FAILED" <<< "$output"; then
+            # no hunk FAILED
+            if grep -q "hunks\? ignored" <<< "$output"; then
+                # some or all hunks are ignored
+                warning "Some/All hunks are ignored when trying to applying $relative, skipping this patch"
+            else
+                git_am "$TOP/$dir" "$patch"
+            fi
+        else
+            # Some hunks failed
+            error "failed to apply $patch!"
+            echo "$output"
+            exit 3
+        fi
+    done
 done
